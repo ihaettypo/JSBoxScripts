935efb0364845fbb8e95dcef17b57d301e807aa8
ihaettypo <annemd3nsi+github@outlook.com>
2019-04-18 12:03:25 +0800

    Update getVXPunchLink.js

--- getVXPunchLink.js
+++ getVXPunchLink.js
@@ -1,7 +1,9 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://raw.githubusercontent.com/ihaettypo/jsQR/master/dist/jsQR.js";
+script.src = "https://min.gitcdn.link/repo/ihaettypo/jsQR/master/dist/jsQR.js";
 script.onload = function() {
+  window.alert(jsQR);
+  window.alert(window.jsQR);
   let qrCanvas = document.querySelector(".ih5-rel-qrcode canvas");
   let qrImageData = qrCanvas.getContext('2d').getImageData(0,0, qrCanvas.width, qrCanvas.height);
   let url = jsQR(qrImageData.data,qrImageData.width,qrImageData.height).data;
